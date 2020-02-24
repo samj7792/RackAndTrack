3ec8c3fe730a59342cb5543c80bd46f6e77e2a0f
Sam Jackson <samuelsjackson7@gmail.com>
2020-02-24 11:50:41 -0500

    connect mongodb with mongoose

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
+// Initialize app variable with express
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Test endpoint
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
