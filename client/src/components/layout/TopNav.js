748d1dd4f5c3959ef81093a505f5baed4ed90304
Sam Jackson <samuelsjackson7@gmail.com>
2020-04-21 09:23:57 -0400

    Logout and Navbar links

--- client/src/components/layout/TopNav.js
+++ client/src/components/layout/TopNav.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const TopNav = () => (
-  <Navbar bg='dark' variant='dark'>
-    <Navbar.Brand href='/' className='mr-auto'>
-      <i className='fas fa-dumbbell' /> Rack and Track
-    </Navbar.Brand>
+const TopNav = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <Nav>
+      <Nav.Item>
+        <a onClick={logout} href='#!'>
+          <i className='fas fas-sign-out-alt' /> <span>Logout</span>
+        </a>
+      </Nav.Item>
+    </Nav>
+  );
+
+  const guestLinks = (
     <Nav>
       <Nav.Item>
         <Nav.Link href='/register'>Register</Nav.Link>
@@ -15,7 +25,27 @@ const TopNav = () => (
         <Nav.Link href='/login'>Log In</Nav.Link>
       </Nav.Item>
     </Nav>
-  </Navbar>
-);
+  );
 
-export default TopNav;
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Navbar.Brand href='/' className='mr-auto'>
+        <i className='fas fa-dumbbell' /> Rack and Track
+      </Navbar.Brand>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </Navbar>
+  );
+};
+
+TopNav.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(TopNav);
